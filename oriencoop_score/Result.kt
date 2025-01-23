02643f8a47273ccfdc0da3e0250e45c4b1a79ec4
Razgriz16 <83658197+Razgriz16@users.noreply.github.com>
2025-01-23 17:17:20 -0300

    Delete oriencoop_score directory

--- oriencoop_score/Result.kt
+++
@@ -1,7 +0,0 @@
-package com.example.oriencoop_score
-
-sealed class Result<out T> {
-    data class Success<out T>(val data: T) : Result<T>()
-    data class Error(val exception: Throwable) : Result<Nothing>()
-    object Loading : Result<Nothing>()
-}
